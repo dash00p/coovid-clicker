9c3539376ab72c3d432d35d9978ef0472838f58d
daShoop <iko.le.kawa@gmail.com>
2024-03-22 23:59:33 +0100

    fix(CI): adjust public ACL for assets

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-aws s3 sync --delete --acl public-read "dist" "s3://${DEPLOY_BUCKET}"
+aws s3 sync --acl public-read --delete "dist" "s3://${DEPLOY_BUCKET}" 
 aws s3 cp --cache-control 'no-cache' --acl public-read "dist/index.html" "s3://${DEPLOY_BUCKET}"
