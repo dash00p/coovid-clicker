21c6d59519bb1f7eec7080754e37d3ea17962a75
daShoop <iko.le.kawa@gmail.com>
2024-04-06 15:59:10 +0200

    feat(CI): add cloudfront service

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,3 +3,4 @@ set -euo pipefail
 
 aws s3 sync --acl public-read --delete "dist" "s3://${DEPLOY_BUCKET}" 
 aws s3 cp --cache-control 'no-cache' --acl public-read "dist/index.html" "s3://${DEPLOY_BUCKET}"
+aws cloudfront create-invalidation --distribution-id "${CLOUDFRONT_DISTRIBUTION_ID}" --paths '/*'
