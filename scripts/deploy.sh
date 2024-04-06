997ba6870e7d1e177b7b40e157fa0e605c6e6af0
daShoop <iko.le.kawa@gmail.com>
2024-04-06 16:08:25 +0200

    fix(CI): remove ACL on website bucket

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-aws s3 sync --acl public-read --delete "dist" "s3://${DEPLOY_BUCKET}" 
-aws s3 cp --cache-control 'no-cache' --acl public-read "dist/index.html" "s3://${DEPLOY_BUCKET}"
+aws s3 sync --delete "dist" "s3://${DEPLOY_BUCKET}" 
+aws s3 cp --cache-control 'no-cache' "dist/index.html" "s3://${DEPLOY_BUCKET}"
 aws cloudfront create-invalidation --distribution-id "${CLOUDFRONT_DISTRIBUTION_ID}" --paths '/*'
